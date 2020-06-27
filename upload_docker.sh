83124b3ade4a7ea51de527a1933bacc66aa68d5a
Raymond Kalonji <raymondkalonji@gmail.com>
2020-06-27 12:00:20 -0400

    feat: upload image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+username=rdaf16
+repository=ml-docker-kube
+tag=rdaf16/api
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/dock_passwd.txt | docker login --username $username --password-stdin 
 
 # Step 3:
 # Push image to a docker repository
+docker push $tag
